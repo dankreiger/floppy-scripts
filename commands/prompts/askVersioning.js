14b4bfae7aa67e5b65713559f7c5bd3e999d7e9e
Dan Kreiger <dan@dankreiger.com>
2019-08-06 22:26:45 +0200

    chore(*): node versioning

--- commands/prompts/askVersioning.js
+++ commands/prompts/askVersioning.js
@@ -2,38 +2,46 @@
 
 const shell = require('shelljs');
 const success = require('./success');
+const chalk = require('chalk');
 const inquirer = require('inquirer');
 
 const askVersioning = () => {
+  const currentNodeVersion = shell.exec('node -v | cut -c 2-').stdout.trim();
+  const currentYarnVersion = shell.exec('yarn -v').stdout.trim();
+  console.log('\n\n');
   const versioningQuestions = [
     {
       type: 'input',
       name: 'nodeVersion',
-      message: "What's your node version?"
+      message: `What's your node version? (${currentNodeVersion})`
     },
     {
       type: 'input',
       name: 'yarnVersion',
-      message: "What's your yarn version?"
+      message: `What's your yarn version? (${currentYarnVersion})`
     }
   ];
 
-  return inquirer.prompt(versioningQuestions).then(answers => {
-    shell.touch(`.nvmrc`);
-    shell.ShellString(`${answers['nodeVersion']}`).to('.nvmrc');
-    success('.nvmrc');
+  return inquirer
+    .prompt(versioningQuestions)
+    .then(({ nodeVersion, yarnVersion }) => {
+      let nv = nodeVersion || currentNodeVersion;
+      let yv = yarnVersion || currentYarnVersion;
+      shell.touch(`.nvmrc`);
+      shell.ShellString(nv).to('.nvmrc');
+      success('.nvmrc');
 
-    shell.exec(`
+      shell.exec(`
     . ~/.nvm/nvm.sh && nvm use
    `);
 
-    shell.exec(
-      `npx json --in-place -f package.json -e 'this.engines={
-        "node": "${answers['nodeVersion']}",
-        "yarn": "${answers['yarnVersion']}"
+      shell.exec(
+        `npx json --in-place -f package.json -e 'this.engines={
+        "node": "${nv}",
+        "yarn": "${yv}"
       }'`
-    );
-  });
+      );
+    });
 };
 
 module.exports = askVersioning;
