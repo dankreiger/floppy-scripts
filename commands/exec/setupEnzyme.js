10fb3d2a946f96eb8bb4d398dde1cc067e029b2e
Dan Kreiger <dan@dankreiger.com>
2019-08-06 19:40:09 +0200

    chore(*): enzyme script

--- commands/exec/setupEnzyme.js
+++ commands/exec/setupEnzyme.js
@@ -2,17 +2,17 @@
 
 const shell = require('shelljs');
 const setupTestsSimple = require('./../../stringConstants/setupTestsSimple');
+const success = require('./../prompts/success');
 
 const setupEnzyme = () => {
-    const baseDir = 'src';
-    const fileName = 'setupTests.js';
+  const baseDir = 'src';
+  const fileName = 'setupTests.js';
 
-    shell.exec('yarn add --dev enzyme enzyme-adapter-react-16');
+  shell.exec('yarn add --dev enzyme enzyme-adapter-react-16');
 
-    shell.touch(`${baseDir}/${fileName}`);
-    shell.ShellString(setupTestsSimple).to(fileName);
-    success(fileName);
-  };
-  
-  module.exports = setupEnzyme;
-  
+  shell.touch(`${baseDir}/${fileName}`);
+  shell.ShellString(setupTestsSimple).to(fileName);
+  success(fileName);
+};
+
+module.exports = setupEnzyme;
