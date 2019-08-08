20a3099a154e463f4f42a685a1e086fd2522c1d7
Dan Kreiger <dan@dankreiger.com>
2019-08-08 03:09:24 +0200

    chore(redux): redux scaffold

--- reduxScaffold.js
+++ reduxScaffold.js
@@ -2,6 +2,8 @@
 
 const { askForReduxScaffoldName } = require('./commands/prompts/askQuestions');
 const makeCodeNice = require('./commands/exec/makeCodeNice');
+const scaffoldReduxState = require('./commands/exec/scaffoldReduxState');
+
 const run = async () => {
   const name = await askForReduxScaffoldName();
   console.log('name', name);
